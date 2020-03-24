4f8570f2184520233a93bb7984bdddc1fc81858a
lysa <lysafichot@gmail.com>
2020-03-24 14:05:24 +0100

    .

--- ex01.js
+++ ex01.js
@@ -1,3 +1,6 @@
+//////// TYPE
+console.log('////////TYPE')
+
 var unNombre = 8.5;
 console.log(typeof unNombre);
 
@@ -8,6 +11,7 @@ var array = ["hello", 8, "world"];
 console.log(typeof array);
 
 var bool = true;
+console.log('bool :', bool)
 
 var obj = {
     name : 'Leonardo',
@@ -20,11 +24,12 @@ console.log('object :', typeof obj)
 if ('21' === 21) {
     console.log('strictement égale')
 }
-else if('21' == 21) {
+else if ('21' == 21) {
     console.log('égale')
 }
 
-console.log(bool)
+//////// SCOPE
+console.log('////////SCOPE GLOBAL VS LOCAL')
 
 let x = 10;
 let y = 9;
@@ -32,18 +37,13 @@ if (x > y) {
     let gamma = 12.7 + y;
     i = gamma * x;
 }
-//console.log(gamma)
+
+//console.log(gamma) -> not defined because is a let variable (scope is in block if)
 
 const myName = 'Lysa';
-
 console.log(myName);
 
-
-
-//////// SCOPE
-
 let globaleVar = 2;
-
 function hello() {
     console.log('je suis la fonction hello')
     console.log(globaleVar);
@@ -53,139 +53,12 @@ function hello() {
 
 function hello2() {
     let globaleVar = 5;
-    console.log(globaleVar)
+    console.log('local globarVar in hello2', globaleVar)
     let localVariable = 'local2';
-    console.log(localVariable)
+    console.log('local in hello2)', localVariable)
 }
 
 hello2();
-
-
 console.log(globaleVar)
-//console.log(localVariable)
+//console.log(localVariable) // no work because is local Variable
 
-
-///// CONDITION
-let isLogin = false;
-
-if (isLogin == true) {
-    console.log('Connected')
-}
-else if (isLogin == 0)
-{
-    console.log('Not Authorized')
-}
-else {
-    console.log('Not value boolean')
-}
-
-// Ternaire
-//isLogin ? console.log('connected') : console.log('Not connected')
-
-let a = 8;
-
-let b = a + 1;
-a = a + 1; //a += 1
-a = a * 2; // a *= 2;
-b = b - 5; // b -= 5
-b = b * b; //b *= b;
-a = a + 3 //a +=3;
-
-console.log('a', a)
-if (a == 1)
-{
-   a = a + 2;
-}
-else {
-  a = a + 3;
-}
-console.log('result', a)
-
-// Boucle
-
-let variable = 0;
-
-while (variable <= 10)
-{
-    console.log(variable);
-   // variable++;
-    variable++;
-   // variable += 1
-}
-
-let tab = ['Yoram', 'Andreia', 'Yassin', 'Miguel'];
-
-
-let myDiv = document.querySelector('.names');
-console.log(myDiv);
-tab.forEach((name) => {
-    console.log(name)
-    myDiv.append(name)
-
-
-})
-
-
-//
-let helloWorld =  "      Hello world; Montrouge   ";
-console.log(helloWorld)
-
-console.log(helloWorld = helloWorld.trim())
-console.log(helloWorld)
-
-helloWorld = helloWorld.split(" ");
-console.log(helloWorld)
-
-
-// Fonction
-//ES5
-function helloW() {
-
-    return true;
-
-}
-
-helloW();
-
-let resultat = helloW();
-console.log(helloW())
-console.log(resultat)
-
-// ES6
-
-const helloWorldES6 = () => {
-    return ['ly', 'lu'];
-}
-
-resultat = helloWorldES6();
-console.log(resultat)
-
-const stringReturn = (str) => {
-    return str;
-}
-
-let name = 'toto';
-
-let myNam = stringReturn(name);
-console.log(myNam);
-
-const concatenation = (str, nombre, str2) => {
-    return str + nombre + str2;
-}
-
-let conca = concatenation("j'ai ", 28, " ans");
-console.log(conca)
-
-
-let text = 'Hello World';
-let text1 = 'Hello World1';
-let text2 = 'Hello World2';
-
-const displayText = (tex) => {
-    console.log('unText')
-}
-
-//displayText();
-
-
-displayText(text2);
---
+++ ex02.js
@@ -0,0 +1,17 @@
+///// CONDITION
+
+let isLogin = false;
+
+if (isLogin == true) {
+    console.log('Connected')
+}
+else if (isLogin == 0)
+{
+    console.log('Not Authorized')
+}
+else {
+    console.log('Not value boolean')
+}
+
+// Ternaire
+//isLogin ? console.log('connected') : console.log('Not connected')
